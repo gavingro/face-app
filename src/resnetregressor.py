35cbc53fdff0d7967a63ec895bb93d78b003bdd9
Gavin Grochowski <grochowskigavin@gmail.com>
2022-04-16 15:11:48 -0700

    Update .score() function with all metrics, update docstrings

--- src/resnetregressor.py
+++ src/resnetregressor.py
@@ -6,7 +6,13 @@ from torch import nn
 import matplotlib.pyplot as plt
 import numpy as np
 
-from .hyperparameters import DEVICE, DROPOUT_RATE, LEARNING_RATE, L2_DECAY_RATE
+from .hyperparameters import (
+    DEVICE,
+    DROPOUT_RATE,
+    LEARNING_RATE,
+    L2_DECAY_RATE,
+    BATCH_SIZE,
+)
 from .data import train_loader, validation_loader
 
 
@@ -86,32 +92,43 @@ class ResNetRegressor(nn.Module):
 
     def fit(
         self,
-        train_loader=train_loader,
-        validation_loader=validation_loader,
-        num_epochs=5,
-        model_name="resnet_age",
-        save_every=1,
-    ):
+        train_loader: torch.utils.data.DataLoader = train_loader,
+        validation_loader: torch.utils.data.DataLoader = validation_loader,
+        num_epochs: int = 5,
+        model_name: str = "resnet_age",
+        save_every: int = None,
+    ) -> None:
         """
         Continuously updates the model with backward()
         for each of the epochs based on some loader of
         training data.
 
         Automatically saves model checkpoints which improve
-        the perfomance of the model as you go for future training.
-        model_name must correspond to an existing folder models/.
+        the perfomance of the model as you go for future training
+        to models/model_name/epoch_n.pth
 
-        Returns
-        -------
-        self.loss_records : np.array
-            A tuple of two (1 x num_epochs) vectors holding the record of
-            losses measured at the end of each epoch.
+
+        Parameters
+        ----------
+        train_loader : torch.utils.data.DataLoader, optional
+            Dataset you want to train your model on, by default train_loader
+        validation_loader : torch.utils.data.DataLoader, optional
+            Dataset you want to check validity on. Does NOT affect gradients / weights
+            of the model, but may be "seen" by batch-normalization, by default validation_loader
+        num_epochs : int, optional
+            Number of epochs to train, by default 5
+        model_name : str, optional
+            Filename to save your model to, by default "resnet_age"
+        save_every : int, optional
+            If give, will save the model every N epochs. Else, it
+            will save the model every time the validation score improves,
+            by default None
         """
         # Make Folder for saving snapshots if doesn't exist:
         self.train()
         if not os.path.exists("models/" + model_name):
             os.makedirs("models/" + model_name)
-        
+
         self.to(DEVICE)
 
         final_epoch = self.total_epochs + num_epochs
@@ -130,7 +147,7 @@ class ResNetRegressor(nn.Module):
                 training_loss = self.backward(inputs, labels)
                 epoch_training_losses[i] = training_loss
                 # Print fit updates
-                if (i + 1) % 25 == 0:
+                if (i + 1) % 50 == 0:
                     now = datetime.now()
                     nowstring = now.strftime("%D %H:%M:%S")
                     print(
@@ -161,39 +178,60 @@ class ResNetRegressor(nn.Module):
 
             # Save Model Snapshot if it out-performs other validation sets
             # Handles also the first iteration where there is only the current loss record.
+            PATH = os.path.join(
+                "models", model_name, ("epoch_" + str(self.total_epochs) + ".pth")
+            )
+            if save_every:
+                if ((self.total_epochs) % save_every) == 0:
+                    torch.save(
+                        {
+                            "epoch": self.total_epochs,
+                            "model_state_dict": self.state_dict(),
+                            "optimizer_state_dict": self.optimizer.state_dict(),
+                            "training_loss": self.training_loss_records,
+                            "validation_loss": self.validation_loss_records,
+                        },
+                        PATH,
+                    )
+            else:
+                if (len(self.validation_loss_records) == 1) or (
+                    epoch_validation_loss > max(self.validation_loss_records[:-1])
+                ):
+                    torch.save(
+                        {
+                            "epoch": self.total_epochs,
+                            "model_state_dict": self.state_dict(),
+                            "optimizer_state_dict": self.optimizer.state_dict(),
+                            "training_loss": self.training_loss_records,
+                            "validation_loss": self.validation_loss_records,
+                        },
+                        PATH,
+                    )
+        return
 
-            # ACTUALLY Lets just save all epochs.
-            # if (len(self.validation_loss_records) == 1)
-            # or (epoch_validation_loss > max(self.validation_loss_records[:-1])):
-
-            if ((self.total_epochs) % save_every) == 0:
-                PATH = (
-                    "models/"
-                    + model_name
-                    + "/"
-                    + "epoch_"
-                    + str(self.total_epochs)
-                    + ".pth"
-                )
-                torch.save(
-                    {
-                        "epoch": self.total_epochs,
-                        "model_state_dict": self.state_dict(),
-                        "optimizer_state_dict": self.optimizer.state_dict(),
-                        "training_loss": self.training_loss_records,
-                        "validation_loss": self.validation_loss_records,
-                    },
-                    PATH,
-                )
-
-        return self.training_loss_records, self.validation_loss_records
-
-    def visualize_training(self):
+    def visualize_training(self, ax=None):
         """
         Plots the loss vs the epochs based on the
         loss_records from a train() call.
+
+        Optionally pass in an plt.axis to add it
+        to an ongoing figure.
+
+
+        Parameters
+        ----------
+        ax : matplotlib axis, optional
+            The axis you'd like to build the plot
+            on (if any), by default None
+
+        Returns
+        -------
+        matplotlib axis
+            An axis object holding the loss over epochs
+            plot.
         """
-        ax = plt.subplot()
+        if not ax:
+            ax = plt.subplot()
         ax.plot(
             range(len(self.training_loss_records)),
             self.training_loss_records,
@@ -208,38 +246,103 @@ class ResNetRegressor(nn.Module):
         )
         ax.legend()
         ax.set_title("Training and Validation Loss vs Epochs")
-        plt.show()
         return ax
 
-    def score(self, input_loader):
+    def score(
+        self,
+        training_loader: torch.utils.data.DataLoader,
+        testing_loader: torch.utils.data.DataLoader,
+        show=False,
+        model_name="resnet_age",
+    ) -> None:
         """
-        Evaluates the accuracy of the trained model on
-        a passed in data input loader.
+        Evaluates the RMSE of the model on both a training and testing dataset,
+        creating a visualization of these predictions vs truth along with
+        the models loss over epochs during training.
 
-        Returns
-        -------
-        MSE : A Float of the Mean Squared Error.
+        If show is true, plots the image and prints metrics. If not,
+        the figure will be saved to metrics/model_name/epoch_n.jpg.
+
+        Parameters
+        ----------
+        training_loader : torch.utils.data.DataLoader
+            The dataset that your model has trained on.
+        testing_loader : torch.utils.data.DataLoader
+            The dataset that your model will be tested on.
+        show : bool, optional
+            Prints out the figure if True, else saves it
+            to a .jpg file if False, by default False
+        model_name : str, optional
+            "name" of model to save the jpg to in filepath,
+            by default "resnet_age"
         """
         self.eval()
         self.to(DEVICE)
         with torch.no_grad():
+            # Create Figures
+            f, axd = plt.subplot_mosaic(
+                [["upper", "upper"], ["lower_left", "lower_right"]], figsize=(10, 8)
+            )
+            plt.suptitle(
+                f"Metrics for {LEARNING_RATE=}, {DROPOUT_RATE=}\n\
+                {L2_DECAY_RATE=}, {BATCH_SIZE=} at epoch {self.total_epochs}"
+            )
+
+            # Make LOSS VS EPOCHS Plot:
+            axd["upper"] = self.visualize_training(axd["upper"])
+
+            # Make TRAINING DATA plot
+            axd["lower_left"].set_ylabel("Observed Ages")
+            axd["lower_left"].set_xlabel("Predicted Ages")
+
             # Get predictions from model, and
             # compare with true labels.
             # Make MSE for each batch, then
             # average them.
-            f = plt.figure()
-            ax = plt.subplot()
-            ax.set_ylabel("Observed Ages")
-            ax.set_xlabel("Predicted Ages")
-            batch_mses = np.empty(len(input_loader))
-            for i, (x, labels) in enumerate(input_loader):
+            batch_mses = np.empty(len(training_loader))
+            for i, (x, labels) in enumerate(training_loader):
                 outputs = self.forward(x)
-                ax.scatter(outputs, labels)
+                axd["lower_left"].scatter(outputs, labels)
                 batch_mse = torch.sum(torch.pow(labels - outputs, 2)) / len(x)
                 batch_mses[i] = batch_mse
+                # Cap at certain amount of points to save time and reduce clutter
+                if i > 50:
+                    break
+            training_rmse = (np.sum(batch_mses) / len(training_loader)) ** (1 / 2)
+            axd["lower_left"].set_title(f"Training RMSE: {training_rmse}")
 
-            total_rmse = (np.sum(batch_mses) / len(input_loader)) ** (1 / 2)
-            plt.show()
-            # Display results for each class
-            print(f"Total RMSE: {total_rmse}")
-        return total_rmse
+            # Make TESTING DATA plot
+            axd["lower_right"].set_xlabel("Predicted Ages")
+
+            # Get predictions from model, and
+            # compare with true labels.
+            # Make MSE for each batch, then
+            # average them.
+            batch_mses = np.empty(len(testing_loader))
+            for i, (x, labels) in enumerate(testing_loader):
+                outputs = self.forward(x)
+                axd["lower_right"].scatter(outputs, labels)
+                batch_mse = torch.sum(torch.pow(labels - outputs, 2)) / len(x)
+                batch_mses[i] = batch_mse
+                # Cap at certain amount of points to save time and reduce clutter
+                if i > 50:
+                    break
+            testing_rmse = (np.sum(batch_mses) / len(testing_loader)) ** (1 / 2)
+            axd["lower_right"].set_title(f"Testing RMSE: {testing_rmse}")
+            plt.tight_layout()
+
+            # Display / save results
+            if show:
+                print(f"RMSE: {training_rmse=}, {testing_rmse=}")
+                plt.show()
+                return
+            else:
+                if not os.path.exists("metrics/" + model_name):
+                    os.makedirs("metrics/" + model_name)
+                PATH = os.path.join(
+                    "metrics",
+                    (model_name),
+                    ("epoch_" + str(self.total_epochs) + ".jpg"),
+                )
+                f.savefig(PATH, bbox_inches="tight")
+                return
