29f087c758859516e4666c876fb13b45feeef729
Gavin Grochowski <grochowskigavin@gmail.com>
2022-04-19 21:45:56 -0700

    Set Seed and Deterministic for reproducibility

--- src/hyperparameters.py
+++ src/hyperparameters.py
@@ -2,12 +2,14 @@ import torch
 
 # Hyperparameters for Model
 BATCH_SIZE = 16
-LEARNING_RATE = 0.00009
+LEARNING_RATE = 0.0009
 L2_DECAY_RATE = 0.0004
 DROPOUT_RATE = 0.4
 
+torch.manual_seed(0)
+
 if torch.cuda.is_available():
-    DEVICE = "cuda" 
+    DEVICE = "cuda"
     torch.backends.cudnn.deterministic = True
 else:
-    DEVICE ="cpu"
+    DEVICE = "cpu"
